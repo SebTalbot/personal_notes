6d032f9b7236632b8738458b383f744f24cabd1d
Sebastien Talbot <moi@sebastientalbot.com>
2017-10-19 22:59:35 -0400

    [React Native / Simple Todo / Main] Add :: Structure and style

--- web/javascript/react/native/simple_todo/app/components/Main.js
+++ web/javascript/react/native/simple_todo/app/components/Main.js
@@ -12,7 +12,32 @@ export default class Main extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Test</Text>
+
+        <View style={styles.header}>
+          <Text style={styles.headerText}>
+            - NOTER -
+          </Text>
+        </View>
+
+        <ScrollView style={styles.scrollContainer}>
+
+        </ScrollView>
+
+        <View style={styles.footer}>
+          <TextInput
+            style={styles.textInput}
+            placeholder='>note'
+            placeholderTextColor='white'
+            underlineColorAndroid='transparent'
+          > </TextInput>
+        </View>
+
+        <TouchableOpacity style={styles.addButton}>
+          <Text style={styles.addButtonText}>
+            +
+          </Text>
+        </TouchableOpacity>
+
       </View>
     );
   }
@@ -21,9 +46,54 @@ export default class Main extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+  },
+  header: {
+    backgroundColor: '#E91E63',
     alignItems: 'center',
     justifyContent: 'center',
+    borderBottomWidth: 10,
+    borderBottomColor: '#ddd',
+  },
+  headerText: {
+    color: 'white',
+    fontSize: 18,
+    padding: 26,
+  },
+  scrollContainer: {
+    flex: 1,
+    marginBottom: 100,
+  },
+  footer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    zIndex: 0,
+  },
+  textInput: {
+    alignSelf: 'stretch',
+    color: '#fff',
+    padding: 20,
+    backgroundColor: '#252525',
+    borderTopWidth: 2,
+    borderTopColor: '#ededed',
+  },
+  addButton: {
+    position: 'absolute',
+    zIndex: 11,
+    right: 20,
+    bottom: 90,
+    backgroundColor: '#E91E63',
+    width: 90,
+    height: 90,
+    borderRadius: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    elevation: 8,
+  },
+  addButtonText: {
+    color: '#fff',
+    fontSize: 24,
   },
 });
 

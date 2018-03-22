245c291371ff0c39ed20e469b61e9e2e45257ae3
Sebastien Talbot <moi@sebastientalbot.com>
2018-03-22 15:47:00 -0400

    Udemy

---
+++ web/javascript/redux/ReduxSimpleStarter/src/components/app.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import BooksList from '../containers/books_list'
+
+export default class App extends Component {
+  render() {
+    return (
+      <BooksList />
+    );
+  }
+}
